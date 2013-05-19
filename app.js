dacec3a1a6916a872a68a38345502b00da0dedac
Thomas Endres <Thomas-Endres@gmx.de>
2013-05-19 21:15:20 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    ejs = require('ejs');
+
+var app = express();
+
+app.configure(function() {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.engine('html', ejs.renderFile);
+    app.use(express.favicon());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function() {
+    app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res) {
+    res.render('index.html', {
+        title: 'Home'
+    });
+});
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
+});
